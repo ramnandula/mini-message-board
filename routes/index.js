77990defc7612f1926161d2f2cf4070f666adb91
Ram Aditya Nandula <nandulram@gmail.com>
2021-11-18 13:07:37 -0600

    Finished app

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,22 @@ const messages = [
     added: new Date()
   }
 ];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+// get form page
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: "Form" });
+});
+
+
+// post form page
+router.post('/new', function (req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.author, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
